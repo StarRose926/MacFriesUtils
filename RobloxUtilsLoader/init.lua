8cd2aa7c286a52dccfb71614b680b8646519a641
StarRose926 <businessburgerparty@gmail.com>
2026-04-10 21:26:02 +0200

    Update init.lua

--- RobloxUtilsLoader/init.lua
+++ RobloxUtilsLoader/init.lua
@@ -3,10 +3,21 @@ Container.Name = 'RobloxUtils'
 
 -- Load Everything 1 by 1, to not crash by loading everything at once.
 local function loadModulesFromBundler(file)
-    return loadstring(game:HttpGet(file))()
+    local gui, req = loadstring(game:HttpGet(file))()
+
+    for _, inst in next, gui:GetChildren() do
+        inst.Parent = Container
+    end
 end
 
 loadModulesFromBundler('https://raw.githubusercontent.com/StarRose926/MacFriesUtils/refs/heads/main/RobloxUtilsLoader/LuauPolyfillMini.lua')
 loadModulesFromBundler('https://raw.githubusercontent.com/StarRose926/MacFriesUtils/refs/heads/main/RobloxUtilsLoader/LuauPolyfill.lua')
 
-return
+-- NOTE: Wait a little to give time for next load! These Chunks are HUGE!
+for i = 1, 6 do
+    loadModulesFromBundler('https://raw.githubusercontent.com/StarRose926/MacFriesUtils/refs/heads/main/RobloxUtilsLoader/ReactDump' .. tostring(i) .. '.lua')
+    task.wait(0.05)
+end
+
+
+return Container
