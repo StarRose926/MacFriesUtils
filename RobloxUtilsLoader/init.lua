3698fe0463497a0262904b13b3cb486df9b08f7c
StarRose926 <businessburgerparty@gmail.com>
2026-04-10 21:33:11 +0200

    Update init.lua

--- RobloxUtilsLoader/init.lua
+++ RobloxUtilsLoader/init.lua
@@ -1,13 +1,30 @@
 local Container = Instance.new('Folder')
 Container.Name = 'RobloxUtils'
 
+local requires = {}
+local global_require = function() end
+
 -- Load Everything 1 by 1, to not crash by loading everything at once.
 local function loadModulesFromBundler(file)
-    local gui, req = loadstring(game:HttpGet(file))()
-
+    local fn = loadstring(game:HttpGet(file))()
+    setfenv(fn, setmetatable({}, {
+        __index = function(self, key)
+            if key == 'require' then
+                return global_require
+            else
+                return getfenv()[key]
+            end
+        end,
+        __newindex = function(self, key, val)
+            getfenv()[key] = val
+        end
+    }))
+    
     for _, inst in next, gui:GetChildren() do
         inst.Parent = Container
     end
+
+    table.insert(requires, req)
 end
 
 loadModulesFromBundler('https://raw.githubusercontent.com/StarRose926/MacFriesUtils/refs/heads/main/RobloxUtilsLoader/LuauPolyfillMini.lua')
@@ -19,5 +36,16 @@ for i = 1, 6 do
     task.wait(0.05)
 end
 
+global_require = function(module)
+    for _, req in next, requires do
+        if req then
+            local ok, res = pcall(req, module)
 
-return Container
+            if ok and res then
+                return res
+            end
+        end
+    end
+end
+
+return Container, global_require
