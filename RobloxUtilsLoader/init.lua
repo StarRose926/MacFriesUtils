458aa623ec8a23e14947ab2acda5ba667b4af378
StarRose926 <businessburgerparty@gmail.com>
2026-04-10 20:49:06 +0200

    Update init.lua

--- RobloxUtilsLoader/init.lua
+++ RobloxUtilsLoader/init.lua
@@ -2,3 +2,11 @@ local Container = Instance.new('Folder')
 Container.Name = 'RobloxUtils'
 
 -- Load Everything 1 by 1, to not crash by loading everything at once.
+local function loadModulesFromBundler(file)
+    return loadstring(game:HttpGet(file))()
+end
+
+loadModulesFromBundler('https://raw.githubusercontent.com/StarRose926/MacFriesUtils/refs/heads/main/RobloxUtilsLoader/LuauPolyfillMini.lua')
+loadModulesFromBundler('https://raw.githubusercontent.com/StarRose926/MacFriesUtils/refs/heads/main/RobloxUtilsLoader/LuauPolyfill.lua')
+
+return
